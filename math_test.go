b43f8c5fee5a6db68dab56532de65d33d5142656
edsonjabastos <edsonjabastos@hotmail.com>
2024-02-15 11:42:56 -0300

    fix:

--- math_test.go
+++ math_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestAdd(t *testing.T) {
-	total := Add(20, 20)
+	total := add(20, 20)
 	if total != 40 {
 		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 40)
 	}
